283f2f6808a20792f128cd37e60f7c2749eee1c8
FuryX625 <serafin.m625@gmail.com>
2022-03-22 10:15:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner Wczytywanie = new Scanner(System.in);
+    int a, b, suma;
+    System.out.println("Podaj liczbę a: ");
+    a=Wczytywanie.nextInt();
+    System.out.println("Podaj liczbę b: ");
+    b=Wczytywanie.nextInt();
+    suma=a+b;
+    double potega=Math.pow(a,b);
+    System.out.println("Suma liczb a oraz b to: "+suma);
+    System.out.println("Potęga liczb a oraz b to: "+potega);
+  }
+}
